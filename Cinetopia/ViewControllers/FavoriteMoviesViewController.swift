0f1a86dbe45a782de3bd555d71ed4e257971430f
Ândriu <andriu_coelho1993@hotmail.com>
2023-12-03 11:10:32 -0300

    aula 1.4

--- Cinetopia/ViewControllers/FavoriteMoviesViewController.swift
+++ Cinetopia/ViewControllers/FavoriteMoviesViewController.swift
@@ -31,7 +31,13 @@ class FavoriteMoviesViewController: UIViewController {
     // MARK: - Class methods
     
     private func setupConstraints() {
-        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
 }
